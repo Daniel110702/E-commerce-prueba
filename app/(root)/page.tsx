0a58ab4de8b185a9d922dd5567d5a0a85ebbc4c9
Daniel Dominguez <danieljdominguezb@hotmail.com>
2023-07-28 09:59:46 -0500

    first commit

---
+++ app/(root)/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+    return (
+       <>
+        <p>Dashboard Component</p>
+        <div>
+            <Button>Click Me</Button>
+        </div>
+       </>
+    )
+}
